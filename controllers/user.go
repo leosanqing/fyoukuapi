457123e3d2a79540e17c14318b6e12eaeebdc77c
leosanqing <stormleo@qq.com>
2020-10-18 22:16:52 +0800

    完成登录、注册接口实现

---
+++ controllers/user.go
@@ -0,0 +1,80 @@
+package controllers
+
+import (
+	"fyoukuapi/models"
+	"github.com/astaxie/beego"
+	"regexp"
+)
+
+type UserController struct {
+	beego.Controller
+}
+
+// 用户注册
+// @router /register/save [post]
+func (c *UserController) SaveRegister() {
+	mobile := c.GetString("mobile")
+	password := c.GetString("password")
+
+	if mobile == "" {
+		c.Data["json"] = ReturnError(4001, "手机号不能为空")
+		c.ServeJSON()
+	}
+	isMatch, _ := regexp.MatchString(`^1([34578])[0-9]\d{8}$`, mobile)
+	if !isMatch {
+		c.Data["json"] = ReturnError(4002, "手机号码格式不正确")
+		c.ServeJSON()
+	}
+	if "" == password {
+		c.Data["json"] = ReturnError(4003, "密码不能为空")
+		c.ServeJSON()
+	}
+
+	userExistByMobile := models.IsUserExistByMobile(mobile)
+	if userExistByMobile {
+		c.Data["json"] = ReturnError(4004, "该手机号已注册")
+		c.ServeJSON()
+	} else {
+		err := models.UserSave(mobile, EncryptPassword(password))
+		if err == nil {
+			c.Data["json"] = ReturnSuccess(0, "注册成功", nil, 0)
+			c.ServeJSON()
+		} else {
+			c.Data["json"] = ReturnError(5000, err)
+			c.ServeJSON()
+		}
+	}
+}
+
+// @router /login/do [*]
+func (c *UserController) LoginDo() {
+	var (
+		mobile   string
+		password string
+	)
+	mobile = c.GetString("mobile")
+	password = c.GetString("password")
+
+	if mobile == "" {
+		c.Data["json"] = ReturnError(4001, "手机号不能为空")
+		c.ServeJSON()
+	}
+	isMatch, _ := regexp.MatchString(`^1(3|4|5|7|8)[0-9]\d{8}$`, mobile)
+	if !isMatch {
+		c.Data["json"] = ReturnError(4002, "手机号码格式不正确")
+		c.ServeJSON()
+	}
+	if "" == password {
+		c.Data["json"] = ReturnError(4003, "密码不能为空")
+		c.ServeJSON()
+	}
+
+	id, name := models.UserLogin(mobile, EncryptPassword(password))
+	if 0 != id {
+		c.Data["json"] = ReturnSuccess(0, "登录成功",
+			map[string]interface{}{"uid": id, "username": name}, 1)
+	} else {
+		c.Data["json"] = ReturnError(4004, "手机号或密码不正确")
+	}
+	c.ServeJSON()
+}
--- main.go
+++ main.go
@@ -3,9 +3,24 @@ package main
 import (
 	_ "fyoukuapi/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	defaultdb := beego.AppConfig.String("defaultdb")
+	err := orm.RegisterDriver("mysql", orm.DRMySQL)
+	if err != nil {
+		panic(err)
+	}
+	err = orm.RegisterDataBase("default", "mysql", defaultdb)
+	if err != nil {
+		panic(err)
+	}
+
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
 	beego.Run()
 }
-
